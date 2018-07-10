f1852d94de1180527ef237061e5ed5f8d820b608
zhengrui <zhengrui@jiangduoduo.com>
2018-07-10 18:02:08 +0800

    库存模块

--- src/test/java/service/LovServiceTest.java
+++ src/test/java/service/LovServiceTest.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +34,8 @@ public class LovServiceTest {
         System.out.println(lovByCondition);
     }
 
+    @Transactional
+    @Rollback
     @Test
     public void testInsert() {
         List<LovVO> lovVOList = new ArrayList<>();
